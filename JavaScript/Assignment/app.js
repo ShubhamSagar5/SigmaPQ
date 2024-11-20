ab492a9ca9917fdbdc64b05b73b5053179099acc
shubham <shubhamsagar628@gmail.com>
2024-11-20 21:13:59 +0530

    Done Assignment Part 2 JS

---
+++ JavaScript/Assignment/app.js
@@ -0,0 +1,131 @@
+//Js Part 2 Assignment 
+
+//Q1 - Create a number variable num with some value. Now , print “good” if the number is divisible by 10 and print “bad” if it is not 
+
+// const num = 30;
+
+// if(num%10 === 0){
+//     console.log('Its Good Num')
+// }else{
+//     console.log("Its Bad Num")
+// }
+
+//--------------------------------------------------------------
+
+
+//Q2 - Take the user's name & age as input using prompts. Then return the following statement to the user as an alert (by substituting their name & age)
+
+// const firstName = prompt('Enter Your name');
+// const age = prompt('Enter your age');
+
+// alert(`${firstName} is ${age} years old`)
+
+
+//--------------------------------------------------------------
+
+
+//Q3 - Write a switch statement to print the months in a quarter.
+// Months in Quarter 1: January, February, March
+// Months in Quarter 2: April, May, June
+// Months in Quarter 3: July, August, September
+// Months in Quarter 4: October, November, December
+// [Use the number as the case value in the switch]
+
+
+
+// const num = 4
+
+// switch(num){
+//     case 1 : {
+//         console.log('January,February,March');
+//         break;
+//     }
+    
+//     case 2 : {
+//         console.log('April,May,June');
+//         break;
+//     }
+    
+//     case 3 : {
+//         console.log('July,August,September');
+//         break;
+//     }
+    
+//     case 4 : {
+//         console.log('October,November,December');
+//         break;
+//     }
+//     default:{
+//         console.log('You select wrong month')
+//     }
+// }
+
+//--------------------------------------------------------------
+
+
+//Q4 - A string is a golden string if it starts with the character ‘A’ or ‘a’ and has a total length greater than 5. For a given string, print if it is golden or not.
+
+// const str = 'appaaa'
+
+// if((str[0] === 'A' || str[0] ==='a') && str.length > 5){
+//     console.log('Golden String')
+// }else{
+//     console.log('Not Golden')
+// }
+
+//--------------------------------------------------------------
+
+
+//Q5 - Write a program to find the largest of 3 numbers.
+
+// let a = 100;
+// let b = 200;
+// let c = 300;
+
+// if(a > b){
+//     if(a > c){
+//         console.log('a is greater Num')
+//     }
+//     else {
+//         console.log('c is greater Num')
+//     }
+// }else {
+//     if(b > c ){
+//         console.log('b is great')
+//     }else{
+//         console.log('c is great')
+//     }
+// }
+
+
+
+
+//--------------------------------------------------------------
+
+
+//Q6- Write a program to check if 2 numbers have the same last digit. Eg: 32 and 47852 have the same last digit i.e. 2.
+
+
+
+// const str1 = a.toLocaleString()
+// const str2 = b.toLocaleString()
+
+// const ans1 = str1[str1.length-1]
+// const ans2 = str2[str2.length-1]
+
+// if(ans1 === ans2){
+//     console.log('Same Last Digit')
+// }else{
+//     console.log('Not Same')
+// }
+
+
+
+// const num1 = 32;
+// const num2 = 47852;
+// if(num1%10 === num2%10){
+//     console.log('Same Last Digit')
+// }else{
+//     console.log('Not Same Last Digit')
+// }
+
