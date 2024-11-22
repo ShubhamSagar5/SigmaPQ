bb9c6f1dc11b034bc098cfaf927cd341bfe6bcd4
shubham <shubhamsagar628@gmail.com>
2024-11-22 20:39:27 +0530

    Todo App Using only Js Done

--- JavaScript/PracticeQuestion/4-app.js
+++ JavaScript/PracticeQuestion/4-app.js
@@ -26,18 +26,54 @@
 
 //Q4 - Guess the favourite movie 
 
-let favourite = 'little krishana'
-let guess = prompt('Enter the movie Name')
+// let favourite = 'little krishana'
+// let guess = prompt('Enter the movie Name')
 
-while(guess !== favourite){
-    if(guess == 'quit'){
+// while(guess !== favourite){
+//     if(guess == 'quit'){
+//         break;
+//     }
+
+//     console.log('wrong');
+//     guess = prompt('Enter movie name again');
+// }
+
+// if(guess == favourite){
+//     console.log('you guess correctly')
+// }
+//--------------------------------------------------------
+
+
+//Q5 - Todo App using js 
+
+let todo  =  [];
+
+let tasks = prompt('Enter Tasks what you ant to do!!')
+while(true){
+
+    if(tasks == 'quit'){
+        console.log('you Choose quit option')
         break;
     }
 
-    console.log('wrong');
-    guess = prompt('Enter movie name again');
-}
+    if(tasks == 'list'){   
+        console.log('---------------------------')  
+        for(let i = 0; i<todo.length ; i++ ){
+            console.log(i + " - "+todo[i]);
+             }
+             console.log('---------------------------')  
 
-if(guess == favourite){
-    console.log('you guess correctly')
+    }else if (tasks == 'add'){
+        let inputTasks = prompt('Enter Your Tasks Name');
+        todo.push(inputTasks);
+        console.log('Tasks Add Successfully')
+
+    }else if (tasks == 'delete'){
+        let indexNum = prompt('Enter Index Numer Of your Tasks');
+        todo.splice(indexNum,1);
+        console.log('Tasks Delete Successfully')
+    }else{
+        console.log('You type wong option');
+    }
+    tasks = prompt('Enter Tasks what you ant to do!!')
 }
