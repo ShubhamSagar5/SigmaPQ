1be0f6ef5ed1bbecd4b41819b9a6fe0e51110f2c
shubham <shubhamsagar628@gmail.com>
2024-11-26 22:31:41 +0530

    Assignment 7 JS Done

---
+++ JavaScript/Assignment/7-app.js
@@ -0,0 +1,32 @@
+//Qs1.Write an arrow function named array Average that accepts an array of numbers and returns the average of those numbers. 
+
+// let arr = [2,1,4,5];
+
+// let arrayAverage = (arr) => {
+//     let sum = 0;
+//     for(num of arr){
+//         sum = sum + num;
+//     }
+//     let ans = sum/arr.length;
+//     return ans;
+// }
+
+// let ans = arrayAverage(arr);
+// console.log(ans) 
+
+//---------------------------------------------------------------------------------------------------
+
+//Q2 - Qs2.Write an arrow function named is Even () that takes a single number as argument and returns if it is even or not. 
+
+// const isEven = (num) => {
+//     if(num%2 == 0){
+//         return 'even';
+//     }else{
+//         return 'odd';
+//     }
+// }
+// console.log(isEven(2)) 
+
+// const isEven = (n) => (n%2 == 0);
+// console.log(isEven(3)) 
+
---
+++ JavaScript/PracticeQuestion/7-app.js
@@ -0,0 +1,18 @@
+//Q1 - Print the square of n by using arrow function 
+
+// let sq = (n)=>{
+//     return n * n;
+// }
+
+// console.log(sq(2));
+
+//Q2 - Print Hello world for 5 time after 2sec 
+
+// let int = setInterval(()=>{
+//     console.log('Hello World');
+
+// },2000);
+
+// setTimeout(() => {
+//     clearInterval(int);
+// }, 10000);
